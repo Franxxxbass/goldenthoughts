42cf52b57a35979dbd38a1f55beb345d6b4c9d0a
Franxxxbass <63103120+Franxxxbass@users.noreply.github.com>
2022-04-04 14:32:55 +0200

    backend done, no styling

--- randomthought/urls.py
+++ randomthought/urls.py
@@ -5,5 +5,10 @@ from randomthought import views
 app_name = 'randomthought'
 
 urlpatterns = [
-    path('', views.thought, name='todays_thought'),
+    path('', views.thought, name='todays-thought'),
+    path('list/', views.thought_list, name='thought-list'),
+    path('create/', views.thought_create_view, name='thought-create'),
+    path('update/<int:pk>/', views.thought_update_view, name='thought-update'),
+    path('delete/<int:pk>/', views.thought_delete_view, name='thought-delete'),
+    path('detail/<int:pk>/', views.thought_detail_view, name='thought-detail'),
 ]
--- randomthought/views.py
+++ randomthought/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from randomthought.models import Sentence
 
@@ -15,3 +15,76 @@ def thought(request):
         }
     )
 
+
+def thought_list(request):
+
+    sentences = Sentence.objects.all()
+
+    return render(
+        request,
+        'randomthought/thought_list.html',
+        context={
+            'sentences': sentences
+        }
+    )
+
+
+def thought_create_view(request):
+    sentence = request.POST.get('sentence')
+
+    if sentence:
+        Sentence.objects.create(body=sentence)
+
+        return redirect('randomthought:thought-list')
+
+    return render(
+        request,
+        'randomthought/thought_form.html'
+    )
+
+
+def thought_update_view(request, pk):
+    sentence = get_object_or_404(Sentence, pk=pk)
+
+    modified_sentence = request.POST.get('sentence')
+    if modified_sentence:
+        sentence.body = modified_sentence
+        sentence.save()
+
+        return redirect('randomthought:thought-list')
+
+    return render(
+        request,
+        'randomthought/thought_form.html',
+        context={
+            'sentence': sentence,
+        }
+    )
+
+
+def thought_delete_view(request, pk):
+    sentence = get_object_or_404(Sentence, pk=pk)
+
+    if request.method == "POST":
+        sentence.delete()
+        return redirect('randomthought:thought-list')
+
+    return render(
+        request,
+        'randomthought/thought_delete_confirm.html',
+        context={
+            'sentence': sentence,
+        }
+    )
+
+
+def thought_detail_view(request, pk):
+    sentence = get_object_or_404(Sentence, pk=pk)
+
+    return render(
+        request,
+        'randomthought/thought_detail.html',
+        context={
+            'sentence': sentence,
+        }
+    )
